760e164ec1a96eca6d2efeadbea61174d6830bbd
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-10-02 00:26:10 -0300

    Project in next.js / mysql

--- src/app/api/products/[id]/route.jsx
+++ src/app/api/products/[id]/route.jsx
@@ -1,10 +1,13 @@
-export async function GET(req, res) {
+
+export async function GET(request, { params }) {
     try {
+        const { id } = params;
+        
         const response = {
-            message: 'Getting one product'
+            message: 'Getting one product:' + id
           };
     
-        return Response.json({ response })
+        return Response.json({ response})
 
     } catch (error) {
       console.error(error);
