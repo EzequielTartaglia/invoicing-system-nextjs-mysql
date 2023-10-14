45c98d28ba69a18aa577aee9d24f51707194c651
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-10-13 22:27:23 -0300

    Conection to DB

--- src/app/api/products/route.jsx
+++ src/app/api/products/route.jsx
@@ -1,14 +1,22 @@
-export async function GET(req, res) {
-    try {
-        const response = {
-            message: 'Getting products'
-          };
-    
-        return Response.json({ response })
+import { pool } from "../../../../config/db";
 
-    } catch (error) {
-      console.error(error);
-      return new Response('Error: Something went wrong', { status: 500 });
+export async function GET(req, res) {
+  try {
+
+    //POST
+    if (req.method === 'POST') {
+      const [response] = await pool.query('DESCRIBE product')
+      return Response.json(response, { status: 200 });
     }
+
+    //GET
+    else {
+      const [response] = await pool.query('DESCRIBE product')
+      return Response.json(response, { status: 200 });
+    }
+
+  } catch (error) {
+    console.error(error);
+    return new Response("Error: Something went wrong", { status: 500 });
   }
-  
+}
