6d18fb6a0f0d2d56c2957c3a7ee4752082472b17
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-10-15 01:06:46 -0300

    working CR of CRUD / products

--- src/app/api/products/[id]/route.jsx
+++ src/app/api/products/[id]/route.jsx
@@ -1,24 +1,37 @@
 import { pool } from "../../../../../config/db";
+import { NextResponse } from "next/server";
 
-export async function GET(req, { params }) {
-    try {
-        const { id } = params;
 
-      //POST
-      if (req.method === 'POST') {
-        const response = await pool.query(``)
-        return Response.json(response[0], { status: 200 });
-      }
-
-      //GET
-      else {
-        const response = await pool.query(`SELECT * FROM product WHERE id = ${id}`)
-        return Response.json(response[0], { status: 200 });
-      }
-
-    } catch (error) {
-      console.error(error);
-      return new Response('Error: Something went wrong', { status: 500 });
-    }
+export async function GET(request, { params }) {
+  try {
+    const result = await pool.query("SELECT * FROM product WHERE id = ?", [
+      params.id,
+    ]);
+    return NextResponse.json(result[0]);
+  } catch (error) {
+    return NextResponse.json({ message: error.message });
   }
-  
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    await pool.query("DELETE FROM product WHERE id = ?", [params.id]);
+    return NextResponse.json({}, { status: 204 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message });
+  }
+}
+
+export async function PUT(request, { params }) {
+  const data = await request.json();
+
+  try {
+    await pool.query("UPDATE product SET ? WHERE id = ?", [data, params.id]);
+    return NextResponse.json({
+      ...data,
+      id: params.id,
+    });
+  } catch (error) {
+    return NextResponse.json({ message: error.message });
+  }
+}
