36f9c1af694563559db4035526c82623f64b5125
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-10-21 20:35:32 -0300

    View

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,26 +1,28 @@
 import axios from 'axios'
+import Buttons from "./Buttons";
+
 
 async function loadProduct(productID){
   const { data } = await axios.get("http://localhost:3000/api/products/"+ productID);
   return data
 }
 
-
-export default async function productID({ params }) {
+async function ProductPage({ params }) {
   const product = await loadProduct(params.id);
   console.log(product)
-  
   return (
-    <div>
-      {product ? (
-        <>
-          <div>Product ID: {product.id}</div>
-          <div>Product Name: {product.name}</div>
-          {/* Add more product details as needed */}
-        </>
-      ) : (
-        <p>Loading product data...</p>
-      )}
-    </div>
+    <section className="flex justify-center items-center h-[calc(100vh-10rem)]">
+      <div className="flex w-4/6 h-2/6 justify-center">
+        <div className="p-6 bg-white w-1/3">
+          <h3 className="text-2xl font-bold mb-3">{product.name}</h3>
+          <h4 className="text-4xl font-bold">{product.price}$</h4>
+          <p className="text-slate-700">{product.description}</p>
+          <Buttons productId={product.id} />
+        </div>
+      </div>
+    </section>
   );
 }
+
+export default ProductPage;
+
