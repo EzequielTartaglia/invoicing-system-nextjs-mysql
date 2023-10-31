607fb889a4d0e4c652cbe170bad3412effe2ba4c
Ezequiel M. Tartaglia <96756615+EzequielTartaglia@users.noreply.github.com>
2023-10-31 18:14:26 -0300

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,12 +1,19 @@
 -- Create DB
 CREATE DATABASE IF NOT EXISTS invoicing_system_db;
 
-
 -- Use schema
 USE invoicing_system_db;
 
-
 -- Create Tables
+CREATE TABLE user(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+--DESCRIBE user;
+
 CREATE TABLE product(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -19,14 +26,12 @@ CREATE TABLE product(
 );
 --DESCRIBE product;
 
-
 CREATE TABLE category(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(400)
 );
 --DESCRIBE category;
-
 
 CREATE TABLE purchase(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -37,13 +42,3 @@ CREATE TABLE purchase(
     CONSTRAINT fk_purchase_user FOREIGN KEY (user_id) REFERENCES user(id)
 );
 --DESCRIBE purchase;
-
-
-CREATE TABLE user(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
---DESCRIBE user;
