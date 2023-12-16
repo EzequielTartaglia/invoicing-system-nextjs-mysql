da683f147d6b6c189ec2e8fba3726884cb8a1b21
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-12-16 12:48:12 -0300

    paginator to products

--- src/views/ProductsPage/ProductTableWithPaginator.jsx
+++ src/views/ProductsPage/ProductTableWithPaginator.jsx
@@ -1,19 +1,19 @@
-"use client";
-import Link from "next/link";
-import { dateFormat } from "@/helpers/dateFormat";
+'use client'
+
+import Link from "next/link"
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight, faAngleDoubleLeft, faAngleDoubleRight, } from "@fortawesome/free-solid-svg-icons";
 
-export default function SaleTableWithPaginator({ validSales }) {
-  const itemsPerPage = 10;
+export default function ProductTableWithPaginator({ validProducts }) {
+    const itemsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(1);
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const visibleSales = validSales.slice(startIndex, endIndex);
+  const visibleProducts = validProducts.slice(startIndex, endIndex);
 
-  const totalPages = Math.ceil(validSales.length / itemsPerPage);
+  const totalPages = Math.ceil(validProducts.length / itemsPerPage);
 
   const goToFirstPage = () => {
     setCurrentPage(1);
@@ -34,52 +34,40 @@ export default function SaleTableWithPaginator({ validSales }) {
   const goToLastPage = () => {
     setCurrentPage(totalPages);
   };
-
-  return (
-    <div>
-      <div className="overflow-x-auto mt-0">
-        <table className="min-w-full border border-gray-300 ">
-          <thead className="bg-blue-500 w-full text-white font-bold">
-            <tr>
-              <th className="py-3 px-6 border-b">Fecha</th>
-              <th className="py-3 px-6 border-b">Usuario</th>
-              <th className="py-3 px-6 border-b">Total</th>
-              <th className="py-3 px-6 border-b">Estado</th>
-              <th className="py-3 px-6 border-b">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {visibleSales.map((sale) => (
-              <tr
-                key={sale.sale_id}
-                className="hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
-              >
-                <td className="py-3 px-6 border-b text-center">
-                  {dateFormat(sale.sale_date)}
-                </td>
-                <td className="py-3 px-6 border-b text-center">
-                  {sale.user_name}
-                </td>
-                <td className="py-3 px-6 border-b text-center">
-                  {sale.sale_total}
-                </td>
-                <td className="py-3 px-6 border-b text-center">
-                  {sale.sale_is_closed ? "Cerrado" : "Abierto"}
-                </td>
-                <td className="py-3 px-6 border-b text-center">
-                  <Link
-                    href={`/sales/${sale.sale_id}`}
-                    className="text-blue-500"
-                  >
-                    Ver
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="flex justify-between items-center mt-4">
+    return (<>
+    <div className="overflow-x-auto">
+    <table className="min-w-full border border-gray-300 ">
+      <thead className="bg-blue-500 w-full text-white font-bold">
+        <tr>
+          <th className="py-3 px-6 border-b">Nombre</th>
+          <th className="py-3 px-6 border-b">Descripción</th>
+          <th className="py-3 px-6 border-b">Categoria</th>
+          <th className="py-3 px-6 border-b">Precio</th>
+          <th className="py-3 px-6 border-b">Unidades en stock</th>
+          <th className="py-3 px-6 border-b">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {visibleProducts.map((product) => (
+          
+          <tr key={product.product_id}  className="hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
+          >
+            <td className="py-3 px-6 border-b text-center">{product.product_name}</td>
+            <td className="py-3 px-6 border-b text-center">{product.product_description}</td>
+            <td className="py-3 px-6 border-b text-center">{product.category_name}</td>
+            <td className="py-3 px-6 border-b text-center">{product.product_price}</td>
+            <td className="py-3 px-6 border-b text-center">{product.product_stock_quantity}</td>
+            <td className="py-3 px-6 border-b text-center">
+            <Link href={`/products/${product.product_id}`} className="text-blue-500">
+              Ver
+            </Link>
+          </td>          
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+        <div className="flex justify-between items-center mt-4">
         <div>
           <button
             onClick={goToFirstPage}
@@ -134,6 +122,6 @@ export default function SaleTableWithPaginator({ validSales }) {
           </button>
         </div>
       </div>
-    </div>
-  );
+      </>
+  )
 }
--- src/views/ProductsPage/ProductsPage.jsx
+++ src/views/ProductsPage/ProductsPage.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Link from "next/link"; 
+import ProductTableWithPaginator from "./ProductTableWithPaginator";
 import Button from "@/components/Button";
 
 async function loadProduct() {
@@ -35,39 +35,8 @@ async function ProductsPage() {
    <div className="container mx-auto flex justify-between items-center">  
   </div>
 
-    <div className="overflow-x-auto">
-  <table className="min-w-full border border-gray-300 ">
-    <thead className="bg-blue-500 w-full text-white font-bold">
-      <tr>
-        <th className="py-3 px-6 border-b">Nombre</th>
-        <th className="py-3 px-6 border-b">Descripción</th>
-        <th className="py-3 px-6 border-b">Categoria</th>
-        <th className="py-3 px-6 border-b">Precio</th>
-        <th className="py-3 px-6 border-b">Unidades en stock</th>
-        <th className="py-3 px-6 border-b">Acciones</th>
-      </tr>
-    </thead>
-    <tbody>
-      {validProducts.map((product) => (
-        
-        <tr key={product.product_id}  className="hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
-        >
-          <td className="py-3 px-6 border-b text-center">{product.product_name}</td>
-          <td className="py-3 px-6 border-b text-center">{product.product_description}</td>
-          <td className="py-3 px-6 border-b text-center">{product.category_name}</td>
-          <td className="py-3 px-6 border-b text-center">{product.product_price}</td>
-          <td className="py-3 px-6 border-b text-center">{product.product_stock_quantity}</td>
-          <td className="py-3 px-6 border-b text-center">
-          <Link href={`/products/${product.product_id}`} className="text-blue-500">
-            Ver
-          </Link>
-        </td>          
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
-
+  <ProductTableWithPaginator validProducts={validProducts}/>
+   
     </div>);
 }
 
