07c4db8ff639dfd31e42c51c4cc219a7342ac70f
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-12-30 16:35:20 -0300

    add product and rest stock

--- src/app/sales/[id]/DeleteAllProductIdButton.jsx
+++ src/app/sales/[id]/DeleteAllProductIdButton.jsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 export default function DeleteAllProductIdButton({ saleId, productId }) {
     const router = useRouter();
 
+    const [productStock, setProductStock] = useState(0);
+
     useEffect(() => {
         const fetchSale = async (id) => {
             try {
@@ -20,16 +22,28 @@ export default function DeleteAllProductIdButton({ saleId, productId }) {
             fetchSale(saleId);
         }
     
-      }, [saleId]);
+        const fetchProduct = async (id) => {
+          try {
+            const { data } = await axios.get('/api/products/' + id);
+            setProductStock(data[0].product_stock_quantity);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+      // Obtener la cantidad de stock del producto al cargar el componente
+      fetchProduct(productId);
+
+      }, [productId, saleId]);
     
+      const handleDeleteAllItems = async () => {
+        const res = await axios.delete(`http://localhost:3000/api/sales/${saleId}/sale_items/${productId}`);
+          router.refresh();
+      }
+
   return (
     <button
         className="text-white bg-red-500 hover:bg-red-700 py-1 px-3 rounded"
-        onClick={async () => {
-            const res = await axios.delete(`http://localhost:3000/api/sales/${saleId}/sale_items/${productId}`);
-              router.refresh();
-        }}
-      >
+        onClick={handleDeleteAllItems}>
         vaciar
       </button>
   )
