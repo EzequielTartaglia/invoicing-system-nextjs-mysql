5f7670d136e492b62b6939315383144cea7102c5
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-12-30 14:56:15 -0300

    Starting feature of login

---
+++ src/app/api/users/[id]/route.jsx
@@ -0,0 +1,44 @@
+import { pool } from "../../../../../config/db";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+
+  try {
+    const { id } = params;
+
+    const result = await pool.query("SELECT * FROM user WHERE user_id = ?;", [
+      id
+    ]);
+    return NextResponse.json(result[0]);
+  } catch (error) {
+    return NextResponse.json({ message: error.message });
+  }
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const { id } = params;
+
+    await pool.query("DELETE FROM user WHERE user_id = ?", [id]);
+
+    return NextResponse.json({}, { status: 204 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message });
+  }
+}
+
+export async function PUT(request, { params }) {
+  const data = await request.json();
+
+  try {
+    const { id } = params;
+
+    await pool.query("UPDATE user SET ? WHERE user_id = ?", [data, id]);
+    return NextResponse.json({
+      ...data,
+      id: params.id,
+    });
+  } catch (error) {
+    return NextResponse.json({ message: error.message });
+  }
+}
--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -1,6 +1,33 @@
+import axios from "axios";
+import Button from "@/components/Button";
 
-export default function LoginPage() {
-  return (
+async function loadUsers() {
+    const { data } = await axios.get("http://localhost:3000/api/users");
+    console.log(data);
+    return data;
+  }
+
+export default async function LoginPage() {
+ 
+    const users = await loadUsers();
+
+    // Filter non banned users 
+    const validNonBannedUsers = users.filter((user) => user.user_id !== undefined && user.user_is_banned !== 1);
+
+    if (validNonBannedUsers.length === 0) {
+        return (
+          <div className="h-5/6 grid place-items-center">
+            <div className="overflow-x-auto mt-[70px]">
+              <h1 className="mb-4 text-center">No se encuentran usuarios registrados en la base de datos.</h1>
+            </div>
+            <div className="items-center mt-[50px]">
+              <Button href="/singin" text="Registrar usuario" />
+            </div>
+          </div>
+        );
+      }
+      
+    return (
     <div>LoginPage</div>
   )
 }
