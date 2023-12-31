41e3786a749221159abf0653de29ac6b0310461e
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-12-31 19:59:33 -0300

    Starting to do userform

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import EzTechLogo from '../../public/ez-tech_logo.webp';
 
 export function LoginForm() {
   const [user, setUser] = useState({
@@ -41,7 +43,6 @@ export function LoginForm() {
     
         if (foundUser) {
           // Encontrado: actualiza la propiedad user_is_active a 1
-          alert("Inicio de sesion exitoso")
           await axios.put(`/api/users/${foundUser.user_id}`, {
             user_email: user.user_email,
             user_password: user.user_password,
@@ -71,17 +72,26 @@ export function LoginForm() {
     
   return (
     <div className="md:min-w-[400px] lg:min-w-[600px]">
+
+
       <form
         className="bg-white dark:bg-gray-800 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 border border-gray-600 shadow-md"
         onSubmit={handleSubmit}
       >
+
+      <div className="mb-4 text-xl font-bold text-center text-gray-800 dark:text-white">
+        <div className="flex items-center justify-center mb-4">
+          <Image src={EzTechLogo} alt="" className="w-[150px] h-[150px] rounded-full" />
+        </div>
+        Ingresar
+      </div>
 
         <div className="mb-4">
           <label
             className="block text-gray-700 dark:text-white text-sm font-bold mb-2"
             htmlFor="email"
           >
-            Usuario
+            Correo electronico
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-slate-900 dark:text-white"
@@ -105,7 +115,7 @@ export function LoginForm() {
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-600 dark:border-slate-900 dark:text-white"
-            type="password"  // Cambié el tipo a password para ocultar la contraseña
+            type="password"
             placeholder="Ingrese una contraseña"
             id="password"
             name="user_password"
@@ -117,7 +127,7 @@ export function LoginForm() {
 
         <div className="flex justify-center">
           <button className="bg-blue-500 hover:bg-blue-700 w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            Ingresar
+          Iniciar sesión
           </button>
         </div>
       </form>
