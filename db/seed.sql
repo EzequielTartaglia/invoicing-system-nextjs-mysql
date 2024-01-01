36c7abc2ec99b13daa398e91aab337ee8bad725d
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-01-01 02:50:11 -0300

    Starting to do userform

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,7 @@
 -- Insert data to get a seed DB
 
 INSERT INTO user (user_name, user_last_name, user_email, user_account, user_password, user_token, user_is_active, user_is_banned, user_created, user_modified) VALUES
-  ('Ezequiel', 'Tartaglia', 'ezequielmtartaglia@gmail.com',"Ezequiel Software Engineer",'123123123',null,FALSE,FALSE,now(),now());
+  ('Ezequiel', 'Tartaglia', 'ezequielmtartaglia@gmail.com',"Ezequiel Software Engineer",'123123123','',FALSE,FALSE,now(),now());
 
 INSERT INTO category (category_name, category_description) VALUES
   ('Bebidas', 'Bebidas con alcohol, bebidas sin alcohol, etc.'),
